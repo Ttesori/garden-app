0810fbaeb70b8e30d9fc4ac1d19c24002305753b
Toni Tesori <tt@tonitesori.dev>
2022-07-30 18:13:54 -0400

    Add logout functionality

--- controllers/index.js
+++ controllers/index.js
@@ -14,7 +14,15 @@ const getRegister = (req, res) => {
 
 // GET /logout
 const getLogout = (req, res) => {
+  req.logout(function (err) {
+    if (err) { return next(err); }
 
+    req.session.destroy((err) => {
+      if (err) console.log('Error : Failed to destroy the session during logout.', err);
+      req.user = null;
+      res.redirect('/');
+    });
+  });
 };
 
 // POST /login
